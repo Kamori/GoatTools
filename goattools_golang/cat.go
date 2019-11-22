22d23a844d12a03d58b0c5401829fbe85ce360a7
tcoil <tcoil@liquidweb.com>
2019-11-22 16:43:44 -0600

    Add cat.go as functional

---
+++ goattools_golang/cat.go
@@ -0,0 +1,57 @@
+// Purpose
+// To mimic the GNU/cat utility for printing file contents
+
+// Lessons learned
+// * Defining a variable in an if statement doesn't allow the variable to act "defined" when compiled. 
+// -- We can get around that by defining the variable and type ahead of time... and/or move the variable
+// -- usage to a function
+// * All individual tools must have a "package main" Note: Having multiple mains per dir is bad practice +
+
+// TODO: implement GNU/cat flags
+// TODO: Change file structure to be more Go best practice, python can reasonably adopt that structure as well
+// -- See: +
+// -- An idea for a golang-refined method could be statically combine all tools into a single package.
+// -- Think Busybox
+
+package main
+
+
+import (
+    "fmt"
+    "os"
+    "log"
+    "bufio"
+)
+
+
+func print_data(file *os.File){
+    scanner := bufio.NewScanner(file)
+
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+
+    }
+
+
+}
+
+func main() {
+    //var file *os.File
+    //var err error
+    if len(os.Args) > 1 {
+	    for _, file_path := range os.Args[1:] {
+		file, err := os.Open(file_path)
+		if err != nil {
+	            log.Fatal(err)
+	        }
+	        defer file.Close()
+		print_data(file)
+	}
+
+    } else {
+	    file := os.Stdin
+	    print_data(file)
+    }
+
+
+}
